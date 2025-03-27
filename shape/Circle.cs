e04cd4298435cb9297bbf18ab80b1127153488bf
nenemizc <emarcellm@gmail.com>
2025-03-28 00:06:03 +0100

    circel

---
+++ shape/Circle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    internal class Circle : Shape
+    {
+        double r;
+
+
+        public Circle(bool isHoley, string color, double r) : base(isHoley, color)
+        {
+            this.r = r;
+        }
+
+        public double R { get => r; set => r = value; }
+
+        public override double Area()
+        {
+            return Math.Pow(r, 2) * Math.PI;
+        }
+
+        public override double Perimeter()
+        {
+            return 2 * r * Math.PI;
+        }
+
+        public override string? ToString()
+        {
+            return base.ToString() + " Kör";
+        }
+    }
+}
