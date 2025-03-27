daeac1a65b69491a3d0088973396dc752a113ef2
nenemizc <emarcellm@gmail.com>
2025-03-28 00:09:16 +0100

    babom

--- shape/Program.cs
+++ shape/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Rectangle rectangle = new Rectangle(false, "red", 1, 2);
+
+            Shape[] shapes = { rectangle, new Square(true, "blue", 1, 1), new Circle(false, "yellow", 5) };
+
+            Square square = new Square(false, "red", 5, 5);
+
+            Rectangle rect = square;
+            Shape shape = square;
+        }
+
+        public Shape RectVagySqr(int width, int height)
+        {
+            if (width == height)
+            {
+                return new Square(false, "blue", width, height);
+            }
+            else
+            {
+                return new Rectangle(false, "blue", width, height);
+            }
+        }
+
+        public Shape MaxArea(Shape[] shapes)
+        {
+            double max = shapes[0].Area();
+            int maxInd = 0;
+
+            foreach (var item in shapes)
+            {
+                if (item.Area() > max)
+                {
+                    max = item.Area();
+                    maxInd = Array.IndexOf(shapes, item);
+                }
+            }
+
+            return shapes[maxInd];
         }
     }
 }
