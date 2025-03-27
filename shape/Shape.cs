c72137a1cd848e0024b40d1fe166e98c47cd6b4b
nenemizc <emarcellm@gmail.com>
2025-03-27 22:53:09 +0100

    sape

---
+++ shape/Shape.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    internal abstract class Shape
+    {
+        public bool isHoley;
+        private string color;
+
+        public string Color { get => color; set => color = value; }
+
+        protected Shape(bool isHoley, string color)
+        {
+            this.isHoley = isHoley;
+            this.color = color;
+        }
+
+        protected Shape(string color)
+        {
+            this.color = color;
+            this.isHoley = false;
+        }
+
+        public void MakeHoley()
+        {
+            this.isHoley = true;
+        }
+
+        public void MakeHoleyIf()
+        {
+            if (!isHoley)
+            {
+                if (Area() > Perimeter())
+                {
+                    isHoley = true;
+                }
+            }
+        }
+
+        public abstract double Perimeter();
+
+        public abstract double Area();
+
+        public override string? ToString()
+        {
+            return $"Szín: {color} lyukas-e: {isHoley} kerület: {Perimeter()} terület: {Area()}";
+        }
+    }
+}
