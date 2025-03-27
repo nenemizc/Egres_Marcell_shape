fdf3eb7d350fabb19bc81aaa80b3538fc1c1e3ef
nenemizc <emarcellm@gmail.com>
2025-03-28 00:04:55 +0100

    rektengol

---
+++ shape/Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    internal class Square : Rectangle
+    {
+        public override double Height { get => Width; set => Height = value; }
+        public override double Width { get => Height; set => Width = value; }
+
+        public Square(bool isHoley, string color, double height, double width) : base(isHoley, color, height, width)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "Négyzet " + base.ToString();
+        }
+    }
+}
